4f2c5ccc2fb14dba31b77d48263b33e1920cdcc7
Akpcodes636 <ewherheakpesiri@gmail.com>
2024-09-30 17:58:43 +0100

    pushing..

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,10 +13,10 @@ import Button from "./ui/Button";
 export default function footer() {
   return (
     <footer className="">
-      <div className="relative min-h-[50vh] px-8 xl:px-44">
+      <div className="relative min-h-[60vh] lg:min-h-[100vh] px-8 xl:px-44">
         <div className="absolute inset-0 flower_background"></div>
-        <div className="absolute bottom-40 flex items-center justify-center">
-          <div className="w-[323px]  md:w-[420x] lg:w-[615px] h-[350px] md:h-[400x] lg:h-[226px] bg-forest-green-500 text-white p-8 rounded-md shadow-lg">
+        <div className="absolute bottom-8 left-1 lg:left-3 lg:bottom-40 flex items-center justify-center">
+          <div className="w-[370px]  md:w-[480px] lg:w-[615px] h-[350px] md:h-[300px] lg:h-[240px] bg-forest-green-500 text-white p-8 rounded-md shadow-lg">
             <h2 className="text-[24px] font-bold mb-4 hero-title">
               Subscribe to our newsletter
             </h2>
@@ -31,7 +31,7 @@ export default function footer() {
                     <IoMailOutline className="text-slate-400" />
                   </div>
                   <input
-                    class="block p-3 pl-10 w-[250px] lg:w-[317px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    class="block p-3 pl-10 w-[310px] lg:w-[317px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Enter your email"
                     type="email"
                     id="email"
---
+++ src/components/landingPage/ProductCard.jsx
@@ -0,0 +1,90 @@
+export default function Product() {
+  return (
+    <section className="container-md mx-auto pt-6 px-3 lg:px-11">
+      <div className="">
+        <div className="flex items-center justify-between gap-2 mb-2">
+          <h1 className="hero-title font-bold text-[28px]">
+            Products on demand
+          </h1>
+          <div className="flex items-center justify-center gap-3">
+            <h1 className="text-forest-green-600 text-[16px]">Sell All</h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4 overflow-scroll md:overflow-scroll lg:overflow-visible">
+        <div className="relative group">
+          <div className="w-[288px] h-[417px] overflow-hidden">
+            <Image
+              alt="images of one of the popular products"
+              width={700}
+              height={700}
+              src={Bambo}
+              className="w-full h-full object-cover rounded-[9px]transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute top-2 left-60 bg-forest-green-500 p-2 rounded-full">
+              <FaRegHeart className="text-white" size={16} />
+            </div>
+          </div>
+          <div>
+            <h3 className="font-normal text-[12px]">Bamboo water bottle</h3>
+            <p className="font-bold text-[12px]">&#8358;500</p>
+          </div>
+        </div>
+        <div className="relative group">
+          <div className="w-[288px] h-[417px] overflow-hidden">
+            <Image
+              alt="images of one of the popular products"
+              width={700}
+              height={700}
+              src={HandKitten}
+              className="w-full h-full object-cover rounded-[9px] transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute top-2 left-60 bg-forest-green-500 p-2 rounded-full">
+              <FaRegHeart className="text-white" size={16} />
+            </div>
+          </div>
+          <div>
+            <h3 className="font-normal text-[12px]">Bamboo water bottle</h3>
+            <p className="font-bold text-[12px]">&#8358;500</p>
+          </div>
+        </div>
+        <div className="relative group">
+          <div className="w-[288px] h-[417px] overflow-hidden">
+            <Image
+              alt="images of one of the popular products"
+              width={700}
+              height={700}
+              src={Soap}
+              className="w-full h-full object-cover rounded-[9px] transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute top-2 left-60 bg-forest-green-500 p-2 rounded-full">
+              <FaRegHeart className="text-white" size={16} />
+            </div>
+          </div>
+          <div>
+            <h3 className="font-normal text-[12px]">Bamboo water bottle</h3>
+            <p className="font-bold text-[12px]">&#8358;500</p>
+          </div>
+        </div>
+        <div className="relative group">
+          <div className="w-[288px] h-[417px] overflow-hidden">
+            <Image
+              alt="images of one of the popular products"
+              width={700}
+              height={700}
+              src={Crochet}
+              className="w-full h-full object-cover rounded-[9px] transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute top-2 left-60 bg-forest-green-500 p-2 rounded-full">
+              <FaRegHeart className="text-white" size={16} />
+            </div>
+          </div>
+          <div>
+            <h3 className="font-normal text-[12px]">Bamboo water bottle</h3>
+            <p className="font-bold text-[12px]">&#8358;500</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
