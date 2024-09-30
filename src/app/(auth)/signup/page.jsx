b7c8997c4b6d45f4db055f52a1947d3549aae7ec
Tubolayefa <tlayefaah@gmail.com>
2024-09-30 17:17:47 +0100

    added new features to improve user experience

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,7 +1,102 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { FaRegEyeSlash } from "react-icons/fa";
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { GoogleLogin } from "@react-oauth/google";
+
+export default function Page() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const initialValues = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Email is required"),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      setLoading(true);
+
+      setTimeout(() => {
+        console.log("Form submitted successfully!", values);
+        setLoading(false);
+
+        router.push(`/otp?email=${values.email}`);
+      }, 3000);
+    },
+  });
+
   return (
-    <div>Signup</div>
-  )
+    <div className="flex flex-col items-center justify-center px-8 md:px-16">
+      <Link href="/">
+        <img src="/images/Logo.png" alt="logo" className="mb-6" />
+      </Link>
+      <div>
+        <h1 className="text-2xl font-bold mb-2">Create your account</h1>
+        <p className="mb-6">
+          Let's get started by creating your account. To keep your account safe,
+          we need a strong password.
+        </p>
+        <form onSubmit={formik.handleSubmit} className="space-y-4 mb-6">
+          <div>
+            <label htmlFor="email" className="block mb-1">
+              Email or Phone Number*
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="w-full px-2 py-3.5 border rounded focus:outline-none"
+              placeholder="Enter your email "
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500">{formik.errors.email}</div>
+            ) : null}
+          </div>
+
+          <Button
+            type="submit"
+            css="text-white bg-forest-green-500 w-full"
+            loading={loading || formik.isSubmitting}
+          >
+            Continue
+          </Button>
+        </form>
+
+        <div className="w-full mx-auto flex justify-center">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </div>
+
+        <p className="mt-6 text-center">
+          Already have an account?{" "}
+          <Link href="/signin" className="text-green-800 hover:underline">
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Loader from "./Loader";
 
-export default function Button({ loading, css, fn, children }) {
+export default function Button({ loading, css, fn, children , type}) {
   return (
     <button
+    type={type}
       onClick={fn}
       disabled={loading}
       className={`${css} flex justify-center items-center gap-1 font-semibold text-white px-3 py-2 rounded-lg min-w-24 hover:scale-[0.96] active:scale-[1.01] duration-150`}
