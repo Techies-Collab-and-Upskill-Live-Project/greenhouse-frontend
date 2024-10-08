09d79671e3d9a06b90299f7156bc0434ab619501
Akpcodes636 <ewherheakpesiri@gmail.com>
2024-10-08 08:54:55 +0100

    push 2 pull

--- src/app/(auth)/otp/page.jsx
+++ src/app/(auth)/otp/page.jsx
@@ -4,17 +4,37 @@ import OtpForm from "@/components/OtpForm";
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlinePermDeviceInformation } from "react-icons/md";
 
 const Page = () => {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
 
-  // console.log(email);
+  // Timer state
+  const [counter, setCounter] = useState(60);
+  const [isTimerActive, setIsTimerActive] = useState(true);
+
+  // Countdown logic
+  useEffect(() => {
+    let timer;
+    if (counter > 0 && isTimerActive) {
+      timer = setTimeout(() => setCounter(counter - 1), 1000);
+    } else {
+      setIsTimerActive(false); // Stop the timer
+    }
+    return () => clearTimeout(timer);
+  }, [counter, isTimerActive]);
+
+  const handleResendOtp = () => {
+    setCounter(60); // Reset timer to 60 seconds
+    setIsTimerActive(true); // Restart the timer
+    // Logic to resend the OTP
+    console.log("Resend OTP");
+  };
 
   return (
-    <section className="flex  justify-center">
+    <section className="flex justify-center">
       <div className="flex flex-col items-center md:items-start justify-center px-6 md:px-16">
         <div className="flex items-center justify-center w-full">
           <Link href="/">
@@ -31,16 +51,24 @@ const Page = () => {
 
           <OtpForm />
 
-          {/* <Button fn={handleSubmit} text="submit" load={loading} /> */}
-          <p className="text-center  mt-4">
+          <p className="text-center mt-4">
             Didn&apos;t receive the verification code? It might take a moment.{" "}
             <span className="block md:inline">
-              You can request a{" "}
+              You can request a new code in{" "}
               <span className="text-green-900">
-                {/* new code in {counter} seconds */}
+                {isTimerActive ? `${counter} seconds` : "now"}.
               </span>
             </span>
           </p>
+
+          {!isTimerActive && (
+            <button
+              onClick={handleResendOtp}
+              className="mt-4 text-blue-500 hover:underline"
+            >
+              Resend OTP
+            </button>
+          )}
         </div>
       </div>
     </section>
