ea47563ab04d7b5faf6a38092f1de5df9893c2df
Ask4mike <mikaelakpang@gmail.com>
2024-10-31 16:34:00 +0100

    fix: repositioned the add to cart button on medium and large screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,7 +103,7 @@ export default function Header() {
             <div className="flex items-center relative cursor-pointer gap-1">
               <BiCart className="text-3xl max-sm:text-3xl" />
               <span className="max-xl:hidden">Cart</span>
-              <span className="absolute -right-1 -top-0.5 flex items-center justify-center bg-[#D42620] h-3 w-3 text-xs rounded-full text-white p-2">
+              <span className="absolute -right-1 md:right-8 lg:right-8 -top-0.5 flex items-center justify-center bg-[#D42620] h-3 w-3 text-xs rounded-full text-white p-2">
                 {cartItems}
               </span>
             </div>
